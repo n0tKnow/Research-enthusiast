6b6ae12d81c23282e381bc3b0b29705f32e6d0fc
n0tKnow <pyinal@mail.ustc.edu.cn>
2022-11-20 01:18:45 +0800

    add alternateDay

--- run.js
+++ run.js
@@ -1,7 +1,7 @@
-$.globalEval(await (await fetch("https://raw.githubusercontent.com/n0tKnow/Research-enthusiast/master/research_enthusiast_browserify.js")).text())
+const [alternateDay] = new Date(new Date().setHours(0, 0, 0, 0) + 2*day*1000).toLocaleString().replaceAll("/","-").split(" ")
 
 const config = {
-    date: "2022-05-26",
+    date: alternateDay,
     targets: "荧光定量PCR仪",
     durations: "8:00-11:30",
 }
