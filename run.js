5f67fa67cc03ca5a47abbeb3754515d6ba43cf99
n0tKnow <pyinal@mail.ustc.edu.cn>
2023-03-24 01:26:35 +0800

    update run script

--- run.js
+++ run.js
@@ -1,7 +1,8 @@
-const [alternateDay] = new Date(new Date().setHours(0, 0, 0, 0) + 2*day*1000).toLocaleString().replaceAll("/","-").split(" ")
+$.globalEval(await (await fetch("https://ghproxy.com/https://raw.githubusercontent.com/n0tKnow/Research-enthusiast/master/research_enthusiast_browserify.js")).text())
+const [date] = new Date(new Date().setHours(0, 0, 0, 0) + 2*day*1000).toLocaleString().replaceAll("/","-").split(" ")
 
 const config = {
-    date: alternateDay,
+    date,
     targets: "荧光定量PCR仪",
     durations: "8:00-11:30",
 }
