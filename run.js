ccf979edcf6cb09ec46233eabe7114bdff86df1d
n0tKnow <pyinal@mail.ustc.edu.cn>
2023-04-14 00:50:56 +0800

    update run.js

--- run.js
+++ run.js
@@ -1,5 +1,5 @@
 $.globalEval(await (await fetch("https://ghproxy.com/https://raw.githubusercontent.com/n0tKnow/Research-enthusiast/master/research_enthusiast_browserify.js")).text())
-const [date] = new Date(new Date().setHours(0, 0, 0, 0) + 2*day*1000).toLocaleString().replaceAll("/","-").split(" ")
+const [date] = new Date(new Date().setHours(0, 0, 0, 0) + 2* 60 * 60 * 24*1000).toLocaleString().replaceAll("/","-").split(" ")
 
 const config = {
     date,
